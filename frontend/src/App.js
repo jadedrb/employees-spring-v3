6dbd07cca142bab5424c6afb7177ce21bece844e
Jade Del Rosario-Bell <JadeDRB@Jades-MBP.lan1>
2020-10-31 17:09:18 -0400

    second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  useEffect(() => {
+    axios.get('/api/employees')
+      .then(res => {
+        console.log('Data: ')
+        console.log(res.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    <div className="App">
+      Test
+    </div>
+  );
+}
+
+export default App;
